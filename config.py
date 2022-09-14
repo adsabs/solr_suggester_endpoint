d2c9ded1a56a995351cc62c49c64a6306bd1003f
tjacovich <tjacovich@cfa.harvard.edu>
2022-09-14 14:37:02 -0400

    Produced operational author_norm endpoint. Added to config file.

--- config.py
+++ config.py
@@ -1,1 +1,24 @@
-SOLR__SUGGEST_URL = "localhost:9983/suggest"
+# encoding: utf-8
+"""
+Configuration file. Please prefix application specific config values with
+the application name.
+"""
+
+import os
+import pwd
+from re import X
+from tkinter import W
+
+LOG_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), './')
+)
+LOG_PATH = '{home}/logs/'.format(home=LOG_PATH)
+LOG_STDOUT = True
+LOGGING_LEVEL = 'DEBUG'
+
+if not os.path.isdir(LOG_PATH):
+    os.mkdir(LOG_PATH)
+
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'unset-env').lower()
+
+SOLR_SUGGEST_URL = "http://localhost:9983/solr/collection1/suggest"
--- solr_suggester/views/suggester_view.py
+++ solr_suggester/views/suggester_view.py
@@ -11,12 +11,36 @@ import requests
 
 class SuggesterView(Resource):
     @staticmethod
-    def query_solr_suggester(query, suggester, **kwargs):
-       return requests.get(url=current_app.config.get("SOLR_URL"))
+    def query_solr_suggester(params):
+       current_app.logger.info("Querying autocomplete with the following parameters: {}".format(params))
+       return requests.get(url=current_app.config.get("SOLR_SUGGEST_URL"), params = params)
     
     @staticmethod
     def restructure_solr_response(solr_response):
         return solr_response
+    
+    @staticmethod
+    def get_GET_params(request, types={}):
+        """
+        Attempt to coerce GET params data into json from request, falling
+        back to the raw data if json could not be coerced.
+        :param request: flask.request
+        :param types: types that the incoming request object must cohere to
+        """
+        try:
+            get_params = request.args.to_dict()
+        except ValueError:
+            msg = "Failed to parse input parameters: {}. Please confirm request is properly formatted.".format(request)
+            raise ValueError(msg)
+        if "q" in get_params.keys():
+            query = get_params.pop("q")
+            current_app.logger.info("Query is {}".format(query))
+            get_params["suggest.q"] = str(query).strip()
+
+            return get_params
+        else:
+            msg = "Query: {} is missing parameter 'q'. Please confirm request conforms to ADS /search syntax.".format(get_params)
+            raise ValueError(msg)
 
 class AuthorSuggesterView(SuggesterView):
     @classmethod
@@ -27,17 +51,37 @@ class AuthorSuggesterView(SuggesterView):
         return 0
 
 class AuthorNormSuggesterView(SuggesterView):
-    @classmethod
-    def query_author_norm_suggester(query, **kwargs):
-        return requests.get()
+    suggester = 'normsuggester'
+    params = {'suggest': 'true', 'suggest.build': 'true', 'suggest.dictionary': suggester, 'suggest.count': '20', 'wt': 'json'}
 
-    def get():
-        return 0
+    @classmethod
+    def query_author_norm_suggester(self, request):
+        user_params = self.get_GET_params(request)
+        for key in user_params.keys():
+            self.params[key] = user_params[key]
+        solr_response = self.query_solr_suggester(params=self.params)
+        
+        if solr_response.status_code == 200:
+            current_app.logger.info("Response is {}".format(solr_response.json()))
+            terms = solr_response.json()['suggest'][self.params['suggest.dictionary']][self.params['suggest.q']]['suggestions']
+            current_app.logger.info("Received suggestions: {}".format(terms))
+            suggestions ={"suggestions": []}
+            for i in terms:
+                suggestions['suggestions'].append(i["term"])
+            current_app.logger.info("Suggestions are : {}".format(suggestions['suggestions']))
+            
+        else:
+            current_app.logger.error("Failed to retrieve suggestions with error: {}".format(solr_response.status_code))
+            suggestions = {"suggestions": []}
+
+        return suggestions
+
+    def get(self):
+        response = self.query_author_norm_suggester(request)
+        return response, 200
+
 
 class UATSuggesterView(SuggesterView):
-    @classmethod
-    def query_uat_suggester(query, **kwargs):
-        return requests.get()
-
-    def get():
-        return 0
+    def get(self):
+        current_app.logger.info("Test endpoint reached")
+        return {"response": "Hit endpoint"}, 200
