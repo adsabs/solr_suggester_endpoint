eb41b3fe84397913d51baf65268125aa4d787975
tjacovich <tjacovich@cfa.harvard.edu>
2022-09-14 21:03:41 -0400

    Added full dev environment for suggesters and microservices including nginx proxy.

--- scripts/solr_entrypoint.sh
+++ scripts/solr_entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-cd /montysolr/build/contrib/examples/adsabs/
-cp /solrconfig.xml ./server/solr/collection1/conf/
+cd /app/montysolr/build/contrib/examples/adsabs/
+cp /app/local/scripts/solrconfig.xml ./server/solr/collection1/conf/
 chmod +x ./bin/solr
-./bin/solr -p 8983
+chmod +x ./bin/post
+./bin/solr -p 8983 -force
+./bin/post -c collection1 /app/local/scripts/json_records/* -p 8983
+
+echo "For interactive access, run in a diferent terminal:"
+echo "  docker exec -it solr_search bash"
+echo "Press CTRL+c to stop"
+tail -f /dev/null
