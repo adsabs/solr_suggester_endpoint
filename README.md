7ffec1462c784c35405f9959106ad5200bfab189
tjacovich <tjacovich@cfa.harvard.edu>
2022-09-19 09:53:36 -0400

    Updated readme, refactored suggester_view.py to condense calls to SOLR /suggest.

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ This service makes calls to the the SOLR /suggest endpoint based on the suggeste
 Each suggester uses a slightly different implementation of the SOLR suggester and takes slightly different inputs.
 The endpoints defined in this service break out each suggester into its own endpoint and provide reasonable defaults for autocomplete functionality.
 
-`docker-compose` can be used to launch a dev environment that includes an nginx reverse proxy, the microservice containing the `/autocomplete` endpoints, and a build of `montysolr` that includes a modified `solrconfig.xml` that adds the suggestions `SearchComponent` and `/suggest` SOLR endpoint.
+### Installation and 
+`docker-compose` can be used to launch a dev environment that includes an NGINX reverse proxy, the microservice containing the `/autocomplete` endpoints, and a build of `montysolr` that includes a modified `solrconfig.xml` that adds the suggestions `SearchComponent` and `/suggest` SOLR endpoint.
 
 ```bash
 #This builds the images for each container. 
@@ -18,4 +19,27 @@ docker-compose build --no-cache
 
 #This launches the services and handles modifying SOLR as well as reading in any records in the json_records folder.
 docker-compose up
+```
+prior to running `docker-compose` The user can specify an `$EXTERNAL_IP` for the NGINX to listen on. This should be the network IP of the device if you would like other services on the network to be able to see the endpoint. If `$EXTERNAL_IP` is not set, the service will listen on `127.0.0.1`.
+
+### Usage
+Queries can be made against the suggester using 
+
+```bash
+curl http://$EXTERNAL_IP:8000/autocomplete/$SUGGESTER_NAME/?q=$QUERY_TEXT&cf=$COLLECTION
+```
+
+Currently the three suggesters are:
+- `author`: Suggests based on the full author name
+- `author_norm`: Suggests based on the normalized author name
+- `keyword`: Suggests based on all ADS keywords currently in SOLR
+
+Both `author` and `author_norm` can have an additional field added, `cf`, that allows the user to filter on the ADS collection they would wish to search (ie. astronomy, physics).
+
+### Query Example
+``` bash
+ curl http://127.0.0.1:8000/autocomplete/author_norm/\?q\=re\&cf\=physics
+
+{"suggestions": ["Rezzolla, L", "Reynolds, C", "Revillet, C", "Reville, V", "Reville, B", "Resconi, E", "Renzi, G", "Renschler, M", "Relethford, B", "Reiss, M", "Reinwardt, S", "Reinhart, M", "Reinhardt, C", "Reimann, R", "Reid, H", "Reichherzer, P", "Reetsong, T", "Reep, J", "Rebolo, R", "Rea, I"]}
+
 ```
--- solr_suggester/views/suggester_view.py
+++ solr_suggester/views/suggester_view.py
@@ -10,14 +10,27 @@ from flask_restful import Resource
 import requests
 
 class SuggesterView(Resource):
-    @staticmethod
-    def query_solr_suggester(params):
-       current_app.logger.info("Querying autocomplete with the following parameters: {}".format(params))
-       return requests.get(url=current_app.config.get("SOLR_SUGGEST_URL"), params = params)
+    @classmethod
+    def query_solr_suggester(self, request):
+        user_params = self.get_GET_params(request)
+        for key in user_params.keys():
+            self.params[key] = user_params[key]
+        solr_response = requests.get(url=current_app.config.get("SOLR_SUGGEST_URL"), params = self.params)
     
-    @staticmethod
-    def restructure_solr_response(solr_response):
-        return solr_response
+        if solr_response.status_code == 200:
+            current_app.logger.info("Response is {}".format(solr_response.json()))
+            terms = solr_response.json()['suggest'][self.params['suggest.dictionary']][self.params['suggest.q']]['suggestions']
+            current_app.logger.debug("Received suggestions: {}".format(terms))
+            suggestions ={"suggestions": []}
+            for i in terms:
+                suggestions['suggestions'].append(i["term"])
+            current_app.logger.info("Suggestions are : {}".format(suggestions['suggestions']))
+            
+        else:
+            current_app.logger.error("Failed to retrieve suggestions with error: {}".format(solr_response.text))
+            suggestions = {"suggestions": []}
+
+        return suggestions
     
     @staticmethod
     def get_GET_params(request, types={}):
@@ -52,90 +65,39 @@ class AuthorSuggesterView(SuggesterView):
     suggester = 'authorsuggester'
     params = {'suggest': 'true', 'suggest.dictionary': suggester, 'suggest.count': '20', 'wt': 'json'}
 
-    @classmethod
-    def query_author_suggester(self, request):
-        user_params = self.get_GET_params(request)
-        for key in user_params.keys():
-            self.params[key] = user_params[key]
-        solr_response = self.query_solr_suggester(params=self.params)
-        
-        if solr_response.status_code == 200:
-            current_app.logger.info("Response is {}".format(solr_response.json()))
-            terms = solr_response.json()['suggest'][self.params['suggest.dictionary']][self.params['suggest.q']]['suggestions']
-            current_app.logger.info("Received suggestions: {}".format(terms))
-            suggestions ={"suggestions": []}
-            for i in terms:
-                suggestions['suggestions'].append(i["term"])
-            current_app.logger.info("Suggestions are : {}".format(suggestions['suggestions']))
-            
-        else:
-            current_app.logger.error("Failed to retrieve suggestions with error: {}".format(solr_response.status_code))
-            suggestions = {"suggestions": []}
-
-        return suggestions
-
     def get(self):
-        response = self.query_author_suggester(request)
-        return response, 200
+        try:
+            response = self.query_solr_suggester(request)
+            return response, 200
+
+        except Exception as e:
+            current_app.logger.exception("Failed to return suggestions with Exception: {}".format(e))
+            return {"Failed to return suggestions with Exception: {}".format(e)}
 
 class AuthorNormSuggesterView(SuggesterView):
     suggester = 'normsuggester'
     params = {'suggest': 'true', 'suggest.dictionary': suggester, 'suggest.count': '20', 'wt': 'json'}
 
-    @classmethod
-    def query_author_norm_suggester(self, request):
-        user_params = self.get_GET_params(request)
-        for key in user_params.keys():
-            self.params[key] = user_params[key]
-        solr_response = self.query_solr_suggester(params=self.params)
-        
-        if solr_response.status_code == 200:
-            current_app.logger.info("Response is {}".format(solr_response.json()))
-            terms = solr_response.json()['suggest'][self.params['suggest.dictionary']][self.params['suggest.q']]['suggestions']
-            current_app.logger.info("Received suggestions: {}".format(terms))
-            suggestions ={"suggestions": []}
-            for i in terms:
-                suggestions['suggestions'].append(i["term"])
-            current_app.logger.info("Suggestions are : {}".format(suggestions['suggestions']))
-            
-        else:
-            current_app.logger.error("Failed to retrieve suggestions with error: {}".format(solr_response.status_code))
-            suggestions = {"suggestions": []}
-
-        return suggestions
-
     def get(self):
-        response = self.query_author_norm_suggester(request)
-        return response, 200
+        try:
+            response = self.query_solr_suggester(request)
+            return response, 200
+            
+        except Exception as e:
+            current_app.logger.exception("Failed to return suggestions with Exception: {}".format(e))
+            return {"Failed to return suggestions with Exception: {}".format(e)}
+
 
 
 class KeywordSuggesterView(SuggesterView):
     suggester = 'keywordsuggester'
     params = {'suggest': 'true', 'suggest.dictionary': suggester, 'suggest.count': '20', 'wt': 'json'}
-
-    @classmethod
-    def query_keyword_suggester(self, request):
-        user_params = self.get_GET_params(request)
-        for key in user_params.keys():
-            self.params[key] = user_params[key]
-        solr_response = self.query_solr_suggester(params=self.params)
-        
-        if solr_response.status_code == 200:
-            current_app.logger.info("Response is {}".format(solr_response.json()))
-            terms = solr_response.json()['suggest'][self.params['suggest.dictionary']][self.params['suggest.q']]['suggestions']
-            current_app.logger.info("Received suggestions: {}".format(terms))
-            suggestions ={"suggestions": []}
-            for i in terms:
-                suggestions['suggestions'].append(i["term"])
-            current_app.logger.info("Suggestions are : {}".format(suggestions['suggestions']))
-            
-        else:
-            current_app.logger.error("Failed to retrieve suggestions with error: {}".format(solr_response.status_code))
-            current_app.logger.error("Failed to retrieve suggestions with error: {}".format(solr_response.text))
-            suggestions = {"suggestions": []}
-
-        return suggestions
         
     def get(self):
-        response = self.query_keyword_suggester(request)
-        return response, 200
+        try:
+            response = self.query_solr_suggester(request)
+            return response, 200
+            
+        except Exception as e:
+            current_app.logger.exception("Failed to return suggestions with Exception: {}".format(e))
+            return {"Failed to return suggestions with Exception: {}".format(e)}
